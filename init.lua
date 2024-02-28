1d43df8b08a178df5b3b755defd3459a249c5d08
Your Name <you@example.com>
2024-02-28 22:37:49 +0100

    terraform fixes and spotify controll

--- init.lua
+++ init.lua
@@ -523,12 +523,7 @@ local servers = {
       enable = false,
     }
   },
-  terraformls = {
-    pattern = { "*.tf", "*.tfvars" },
-    callback = function()
-      vim.lsp.buf.format()
-    end
-  },
+  terraformls = {},
   vuels = { filetypes = { 'js' } },
   tailwindcss = {},
   bashls = {},
@@ -615,6 +610,17 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+--
+-- OWN COMMANDS
+--
+function SPOTIFY(command, args) os.execute("playerctl -p spotify " .. command .. " " .. args.args) end
+
+vim.api.nvim_create_user_command('Splay', function(args) SPOTIFY("play", args) end, { nargs = "?" })
+vim.api.nvim_create_user_command('Spause', function(args) SPOTIFY("pause", args) end, { nargs = "?" })
+vim.api.nvim_create_user_command('Snext', function(args) SPOTIFY("next", args) end, { nargs = "?" })
+vim.api.nvim_create_user_command('Sprev', function(args) SPOTIFY("previous", args) end, { nargs = "?" })
+vim.api.nvim_create_user_command('Svol', function(args) SPOTIFY("volume", args) end, { nargs = "?" })
 
 
 print("hello from the cat factory. Setup done!")
